ebe66ec79022d612668c52de959d659e5deadb2c
Ernest <ernest.surys@gmail.com>
2020-06-20 15:15:46 +0200

    cleanup

--- Runtime/VisualElementExtensions.cs
+++ Runtime/VisualElementExtensions.cs
@@ -6,21 +6,14 @@ namespace QuickEye.UIToolkit
 {
     public static class VisualElementExtensions
     {
-        public static void Q<T>(this VisualElement ve, out T element, string name, string className = null) where T : VisualElement
-        {
-            element = ve.Q<T>(name, className);
-            if (element == null)
-            {
-                Debug.LogWarning($"Could not find: {name}, {className}");
-            }
-        }
-
-        public static void InitField<T>(this VisualElement ve, INotifyValueChanged<T> element, EventCallback<ChangeEvent<T>> onValueChange, T initialValue)
+        public static void InitField<T>(this VisualElement ve, INotifyValueChanged<T> element,
+            EventCallback<ChangeEvent<T>> onValueChange, T initialValue)
         {
             InitField(ve, element, onValueChange, () => initialValue);
         }
 
-        public static void InitField<T>(this VisualElement ve, INotifyValueChanged<T> element, EventCallback<ChangeEvent<T>> onValueChange, Func<T> getInitialValue)
+        public static void InitField<T>(this VisualElement ve, INotifyValueChanged<T> element,
+            EventCallback<ChangeEvent<T>> onValueChange, Func<T> getInitialValue)
         {
             element.RegisterValueChangedCallback(onValueChange);
 
