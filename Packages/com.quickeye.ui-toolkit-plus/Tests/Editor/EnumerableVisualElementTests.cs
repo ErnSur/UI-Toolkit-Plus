468756f2940136a1209fa4c51cdb5a5fe0ee4e0f
Ernest <ernest.surys@gmail.com>
2020-06-27 14:27:18 +0200

    Added Enumerable tests

---
+++ Packages/com.quickeye.ui-toolkit-plus/Tests/Editor/EnumerableVisualElementTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace QuickEye.UIToolkit.Tests
+{
+    public class EnumerableVisualElementTests
+    {
+        [Test]
+        public void EnumerableIsNotNull()
+        {
+            var ve = new EnumerableVisualElement();
+            var ve2 = new EnumerableVisualElement();
+            
+            Assert.AreNotEqual(ve,ve2);
+        }
+    }
+}
