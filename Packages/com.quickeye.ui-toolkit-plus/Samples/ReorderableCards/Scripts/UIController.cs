e99e963307341ff8a7713866e19c99ffac178753
Ernest Suryś <ernest.surys@gmail.com>
2022-07-03 14:59:17 +0200

    added cards samples

---
+++ Packages/com.quickeye.ui-toolkit-plus/Samples/ReorderableCards/Scripts/UIController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace QuickEye.UIToolkit.Samples.Cards
+{
+    public class UIController : MonoBehaviour
+    {
+        private VisualElement[] cards;
+        private VisualElement root;
+        void Start()
+        {
+            var uidoc = GetComponent<UIDocument>();
+            root = uidoc.rootVisualElement;
+            GetCards();
+            AddReorderables();
+        }
+
+        private void AddReorderables()
+        {
+            foreach (var card in cards)
+            {
+                card.AddManipulator(new Reorderable());
+            }
+        }
+
+        private void GetCards()
+        {
+            cards = root.Query(null, "card").Build().ToArray();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
