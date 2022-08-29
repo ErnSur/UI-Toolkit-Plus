c2d911b07cf8d2c93b4619732aa32ff55243ff03
Shaun-Fong <774576415@qq.com>
2022-08-29 10:29:45 +0800

    fix: fix type missing errors when building project.

--- Packages/com.quickeye.ui-toolkit-plus/Runtime/AssemblyInfo.cs
+++ Packages/com.quickeye.ui-toolkit-plus/Runtime/AssemblyInfo.cs
@@ -1,5 +1,7 @@
-﻿using System.Runtime.CompilerServices;
+﻿#if UNITY_EDITOR
+using System.Runtime.CompilerServices;
 using UnityEditor.UIElements;
 
 [assembly:UxmlNamespacePrefix("QuickEye.UIToolkit","quick-eye")]
 [assembly:InternalsVisibleTo("QuickEye.UIToolkit.Editor.Tests")]
+#endif
