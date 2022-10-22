f03475598e27880c4d2d23275e9c95e553516fde
Ernest Suryś <ernest.surys@gmail.com>
2022-10-22 15:34:23 +0200

    version bump

--- Packages/com.quickeye.ui-toolkit-plus/Runtime/PackageInfo.cs
+++ Packages/com.quickeye.ui-toolkit-plus/Runtime/PackageInfo.cs
@@ -4,7 +4,7 @@ namespace QuickEye.UIToolkit
 {
     internal static class PackageInfo
     {
-        public const string Version = "1.6.1";
+        public const string Version = "1.7.0";
         public const string Name = "com.quickeye.ui-toolkit-plus";
         public const string DisplayName = "UI Toolkit Plus";
     }
