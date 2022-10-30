3840d0c3a107e37dd3d57c1e2ed41fe518e2d127
Ernest Suryś <ernest.surys@gmail.com>
2022-10-30 22:29:45 +0100

    fixed tab group scroller

--- Packages/com.quickeye.ui-toolkit-plus/Runtime/TabGroup.cs
+++ Packages/com.quickeye.ui-toolkit-plus/Runtime/TabGroup.cs
@@ -1,11 +1,23 @@
+using System.Collections.Generic;
 using UnityEngine.UIElements;
 
 namespace QuickEye.UIToolkit
 {
-    // TODO: Add: Selected Tab Property
     public class TabGroup : VisualElement
     {
         public const string ClassName = "tab-group";
+        private Scroller _horizontalScroller;
+        private TabGroupMode _mode;
+
+        public TabGroup()
+        {
+            CreateScrollView();
+            CreateHorizontalScroller();
+
+            Mode = TabGroupMode.Horizontal;
+            this.InitResources();
+            AddToClassList(ClassName);
+        }
 
         public ScrollView ScrollView { get; private set; }
         public override VisualElement contentContainer => ScrollView.contentContainer;
@@ -20,48 +32,51 @@ namespace QuickEye.UIToolkit
             }
         }
 
-        private TabGroupMode _mode;
-
-        public TabGroup()
-        {
-            CreateScrollView();
-
-            ScrollView.style.flexGrow = 1;
-            Mode = TabGroupMode.Horizontal;
-            this.InitResources();
-            AddToClassList(ClassName);
-        }
+        private float ScrollableWidth =>
+            ScrollView.contentContainer.worldBound.width - ScrollView.contentViewport.layout.width;
 
         private void CreateScrollView()
         {
             ScrollView = new ScrollView();
             ScrollView.RegisterCallback<GeometryChangedEvent>(OnScrollViewGeometryChange);
+            ScrollView.horizontalScrollerVisibility = ScrollerVisibility.Hidden;
             ScrollView.horizontalScroller.valueChanged += OnHorizontalScrollerValueChanged;
             ScrollView.horizontalPageSize = 2;
-            ScrollView.horizontalScroller.lowButton.SetAction(ScrollView.horizontalScroller.ScrollPageUp, 0, 1);
-            ScrollView.horizontalScroller.highButton.SetAction(ScrollView.horizontalScroller.ScrollPageDown, 0, 1);
-            ScrollView.horizontalScroller.pickingMode = PickingMode.Ignore;
+            ScrollView.style.flexGrow = 1;
 
             hierarchy.Add(ScrollView);
         }
 
+        private void CreateHorizontalScroller()
+        {
+            _horizontalScroller = new Scroller();
+            _horizontalScroller.pickingMode = PickingMode.Ignore;
+            _horizontalScroller.direction = SliderDirection.Horizontal;
+            _horizontalScroller.lowButton.SetAction(ScrollView.horizontalScroller.ScrollPageUp, 0, 1);
+            _horizontalScroller.highButton.SetAction(ScrollView.horizontalScroller.ScrollPageDown, 0, 1);
+            _horizontalScroller.AddToClassList("tab-group__scroller");
+            hierarchy.Add(_horizontalScroller);
+
+            UpdateHorizontalScrollerVisibility();
+        }
+
         private void OnScrollViewGeometryChange(GeometryChangedEvent evt)
         {
-            UpdateScrollButtonVisibility();
+            UpdateHorizontalScrollerVisibility();
         }
 
         private void OnHorizontalScrollerValueChanged(float value)
         {
-            UpdateScrollButtonVisibility();
+            UpdateHorizontalScrollerVisibility();
         }
 
-        private void UpdateScrollButtonVisibility()
+        private void UpdateHorizontalScrollerVisibility()
         {
             var horizontalValue = ScrollView.horizontalScroller.value;
             var leftEnabled = horizontalValue != 0;
-            var rightEnabled = horizontalValue < ScrollView.horizontalScroller.highValue;
-            ScrollView.horizontalScroller.lowButton.ToggleDisplayStyle(leftEnabled);
-            ScrollView.horizontalScroller.highButton.ToggleDisplayStyle(rightEnabled);
+            var rightEnabled = horizontalValue < ScrollableWidth;
+            _horizontalScroller.lowButton.ToggleDisplayStyle(leftEnabled);
+            _horizontalScroller.highButton.ToggleDisplayStyle(rightEnabled);
         }
 
         private void SetLayoutMode(TabGroupMode mode)
@@ -69,12 +84,29 @@ namespace QuickEye.UIToolkit
 #if UNITY_2021_2_OR_NEWER
             ScrollView.mode = mode == TabGroupMode.Horizontal ? ScrollViewMode.Horizontal : ScrollViewMode.Vertical;
             ScrollView.verticalScrollerVisibility =
-                mode == TabGroupMode.Horizontal ? ScrollerVisibility.Hidden : ScrollerVisibility.Auto; 
+                mode == TabGroupMode.Horizontal ? ScrollerVisibility.Hidden : ScrollerVisibility.Auto;
 #endif
             ScrollView.style.alignItems = mode == TabGroupMode.Vertical ? Align.FlexStart : Align.FlexEnd;
         }
 
-        private class UxmlFactory : UxmlFactory<TabGroup> { }
+        private new class UxmlFactory : UxmlFactory<TabGroup, UxmlTraits> { }
+
+        public new class UxmlTraits : VisualElement.UxmlTraits
+        {
+            private readonly UxmlEnumAttributeDescription<TabGroupMode> _mode = new() { name = "mode" };
+
+            public override IEnumerable<UxmlChildElementDescription> uxmlChildElementsDescription
+            {
+                get { yield break; }
+            }
+
+            public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
+            {
+                base.Init(ve, bag, cc);
+                var group = (TabGroup)ve;
+                group.Mode = _mode.GetValueFromBag(bag, cc);
+            }
+        }
     }
 
     public enum TabGroupMode
