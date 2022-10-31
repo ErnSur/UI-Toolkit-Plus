6b83de3afd94d75b44ac8ad71dc02f9ae5de50bc
Ernest Suryś <ernest.surys@gmail.com>
2022-10-31 07:34:18 +0100

    Added changelog

--- Packages/com.quickeye.ui-toolkit-plus/Runtime/PackageInfo.cs
+++ Packages/com.quickeye.ui-toolkit-plus/Runtime/PackageInfo.cs
@@ -4,7 +4,7 @@ namespace QuickEye.UIToolkit
 {
     internal static class PackageInfo
     {
-        public const string Version = "1.8.0";
+        public const string Version = "1.9.0";
         public const string Name = "com.quickeye.ui-toolkit-plus";
         public const string DisplayName = "UI Toolkit Plus";
     }
